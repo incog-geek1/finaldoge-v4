7990f13a2d82d10dc9c1993eacf192f21b8d18ea
incog-geek1 <151454755+incog-geek1@users.noreply.github.com>
2024-04-11 21:57:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,67 @@
-import express from 'express'
-import basicAuth from 'express-basic-auth'
-import http from 'node:http'
-import { createBareServer } from '@tomphttp/bare-server-node'
-import path from 'node:path'
-import cors from 'cors'
-import config from './config.js'
-const __dirname = process.cwd()
-const server = http.createServer()
-const app = express(server)
-const bareServer = createBareServer('/o/')
-const PORT = process.env.PORT || 8080
-if (config.challenge) {
-  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
-  console.log('Passwords are: ' + Object.values(config.users))
+import express from "express";
+import http from "node:http";
+import path from "node:path";
+import createBareServer from "@tomphttp/bare-server-node";
 
-  app.use(
-    basicAuth({
-      users: config.users,
-      challenge: true,
-    })
-  )
-}
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'static')))
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
 
-if (config.routes !== false) {
-  const routes = [
-    { path: '/ap', file: 'apps.html' },
-    { path: '/g', file: 'games.html' },
-    { path: '/s', file: 'settings.html' },
-    { path: '/t', file: 'tabs.html' },
-    { path: '/p', file: 'go.html' },
-    { path: '/', file: 'index.html' },
-  ]
-
-  routes.forEach((route) => {
-    app.get(route.path, (req, res) => {
-      res.sendFile(path.join(__dirname, 'static', route.file))
-    })
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-}
+);
 
-if (config.local !== false) {
-  app.get('/e/*', (req, res, next) => {
-    const baseUrls = [
-      'https://raw.githubusercontent.com/v-5x/x/fixy',
-      'https://raw.githubusercontent.com/ypxa/y/main',
-      'https://raw.githubusercontent.com/ypxa/w/master',
-    ]
-    fetchData(req, res, next, baseUrls)
-  })
-}
+app.use(express.static(path.join(__dirname, "static")));
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/index.html'));
+});
+app.get('/student', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/loader.html'));
+});
+app.get('/apps', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/apps.html'));
+});
+app.get('/gms', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/gms.html'));
+});
+app.get('/lessons', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/agloader.html'));
+});
+app.get('/credits', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/credits.html'));
+});
+app.get('/partners', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/partners.html'));
+});
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.sendFile(path.join(process.cwd(), './static/404.html'))
+});
 
-const fetchData = async (req, res, next, baseUrls) => {
-  try {
-    const reqTarget = baseUrls.map((baseUrl) => `${baseUrl}/${req.params[0]}`)
-    let data
-    let asset
-
-    for (const target of reqTarget) {
-      asset = await fetch(target)
-      if (asset.ok) {
-        data = await asset.arrayBuffer()
-        break
-      }
-    }
-
-    if (data) {
-      res.end(Buffer.from(data))
-    } else {
-      next()
-    }
-  } catch (error) {
-    console.error('Error fetching:', error)
-    next(error)
-  }
-}
-
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res)
+    bareServer.routeRequest(req, res);
   } else {
-    app(req, res)
+    app(req, res);
   }
-})
+});
 
-server.on('upgrade', (req, socket, head) => {
+server.on("upgrade", (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head)
+    bareServer.routeUpgrade(req, socket, head);
   } else {
-    socket.end()
+    socket.end();
   }
-})
+});
 
-server.on('listening', () => {
-  console.log(`Running at http://localhost:${PORT}`)
-})
+server.on("listening", () => {
+  console.log(`Doge Unblocker has sucessfully started!\nListening on localhost (Port 8000).`);
+});
 
 server.listen({
-  port: PORT,
-})
+  port: 8000,
+});
